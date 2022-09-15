a5af7a3343d54d58215f803c8959436f47077645
qa_python <alexandr86@live.ru>
2022-09-15 22:10:50 +0300

    add 6 less

--- less3.py
+++ less3.py
@@ -1,46 +1,56 @@
 from collections import deque
 from timeit import timeit
-from random import randint
 
-dq = deque()
-lst = list()
-my_lst = [i for i in range(200, 300)]
+test_lst = [i for i in range(1, 50)]
+my_lst = [i for i in range(1, 1000)]
+test_dq = deque(i for i in range(1, 1000))
 
-# тест 1
-print('append')
-print(timeit('for i in range(100): test_dq.append(i)', globals=globals(), number=10000))
-print(timeit('for i in range(100): test_lst.append(i)', globals=globals(), number=10000))
-print('extend')
-print(timeit('for i in range(100): test_dq.extend(my_lst)', globals=globals(), number=10000))
-print(timeit('for i in range(100): test_lst.append(my_lst)', globals=globals(), number=10000))
-print('pop')
-print(timeit('for i in range(100): test_dq.pop()', globals=globals(), number=10000))
-print(timeit('for i in range(100): test_lst.pop()', globals=globals(), number=10000))
 
-dq.clear()
-lst.clear()
+print('-----------------------------deque.append vs list.append')
+print(timeit('for i in range(100): test_dq.append(i)', globals=globals(), number=5000))
+print(timeit('for i in range(100): test_lst.append(i)', globals=globals(), number=5000))
+print('-----------------------------deque.pop vs list.pop')
+print(timeit('for i in range(100): test_dq.pop()', globals=globals(), number=5000))
+print(timeit('for i in range(100): test_lst.pop()', globals=globals(), number=5000))
+print('-----------------------------deque.extend vs list.extend')
+print(timeit('for i in range(100): test_dq.extend(my_lst)', globals=globals(), number=5000))
+print(timeit('for i in range(100): test_lst.extend(my_lst)', globals=globals(), number=5000))
+"""
+    В операциях "добавление элемента справа", "удаление и возврат элемента справа", "расширение
+правой стороны с добавлением элементов заданного списка" по всем позициям лидирует "deque".
+    Думаю, это обусловлено тем, что 'deque' поддерживает поточно-ориентированные, эффективные 
+по памяти операции добавления и извлечения элементов последовательности с любой стороны с  
+производительностью O(1) в любом направлении.
+    Списки, поддерживающие аналогичные операции, оптимизированы только для быстрых операций с 
+последовательностями фиксированной длины и требуют затрат O(n) на перемещение памяти для операций,
+которые изменяют размер и положение базового представления данных.
+(Подсказчик: docs-python.ru. Нам же главное разобраться, да?)
+"""
 
-# тест 2
-print('appendleft')
-print(timeit('for i in range(100): test_dq.appendleft(i)', globals=globals(), number=1000))
-print(timeit('for i in range(100): test_lst.insert(0, i)', globals=globals(), number=1000))
-print('extendleft')
-print(timeit('test_dq.extendleft(my_lst)', globals=globals(), number=10000))
-print(timeit('my_lst + test_lst', globals=globals(), number=10000))
-print('popleft')
-print(timeit('for i in range(10): test_dq.pop()', globals=globals(), number=10000))
-print(timeit('for i in range(10): test_lst.pop(0)', globals=globals(), number=10000))
 
-dq.clear()
-lst.clear()
+print('-----------------------------deque.appendleft vs list.insert(0...)')
+print(timeit('for i in range(5): test_dq.appendleft(i)', globals=globals(), number=10))
+print(timeit('for i in range(5): test_lst.insert(0, i)', globals=globals(), number=10))
+print('-----------------------------deque.extendleft vs list."конкатенация"')
+print(timeit('test_dq.extendleft(my_lst)', globals=globals(), number=10))
+print(timeit('my_lst + test_lst', globals=globals(), number=10))
+print('-----------------------------deque.popleft vs list.pop(0)')
+print(timeit('for i in range(10): test_dq.popleft()', globals=globals(), number=10))
+print(timeit('for i in range(10): my_lst.pop()', globals=globals(), number=10))
+"""
+    В операциях "добавление элемента слева", "расширение левой стороны с добавлением элементов 
+заданного списка" скорость снова на стороне "deque".
+    Обычный список, "притормаживает" за счёт постоянного пересчёта индексов составляющих его
+членов.
+    "Удаление и возврат элемента слева" - на стороне списка.
+"""
 
-# тест 3
-for i in range(101):
-    lst.append(i)
 
-for i in range(101):
-    dq.append(i)
-
-print('Операция получения элемента')
-print(timeit('for i in range(100): test_dq[randint(0, 100)]', globals=globals(), number=10000))
-print(timeit('for i in range(100): test_lst[randint(0, 100)]', globals=globals(), number=10000))
+print('-----------------------------Получение элементов по индексу (deque[..] vs list[..])')
+print(timeit('for i in range(100): test_dq[i]', globals=globals(), number=10000))
+print(timeit('for i in range(100): test_lst[i]', globals=globals(), number=10000))
+"""
+   На большой выборке и заявленных операциях "deque" имеет пеимущества.
+Замеры на небольших параметрах показывают преиущесво "штатных" методов.
+    Вывод: Всегда надо действовать по обстановке). 
+"""
--- less4.py
+++ less4.py
@@ -1,18 +1,23 @@
-
-from timeit import timeit
 from collections import OrderedDict
+from timeit import timeit
 
-o_dct = OrderedDict()
-dct = dict()
+my_dict = {}
+ord_dict = OrderedDict()
 
-print('Заполнение словарей')
-print(timeit('for i in range(1000): o_dct[i] = i', globals=globals(), number=1000))
-print(timeit('for i in range(1000): dct[i] = i', globals=globals(), number=1000))
-
-print('Получение значений по ключу')
-print(timeit('for i in range(1000): a = o_dct[i]', globals=globals(), number=1000))
-print(timeit('for i in range(1000): b = dct[i]', globals=globals(), number=1000))
-
-print('Удаление элемента')
-print(timeit('for _ in range(10): o_dct.popitem()', globals=globals(), number=100))
-print(timeit('for _ in range(10): dct.popitem()', globals=globals(), number=100))
+print('_____________________ Добавление элементов в словари: dict vs OrderedDict')
+print(timeit('for i in range(1000): my_dict[i] = i', globals=globals(), number=10000))
+print(timeit('for i in range(1000): ord_dict[i] = i', globals=globals(), number=10000))
+# Стандартный метод впереди.
+print('_____________________ Получение значений элементов словарей: dict vs OrderedDict')
+print(timeit('for k in my_dict: my_dict.get(k)', globals=globals(), number=10000))
+print(timeit('for k in ord_dict: ord_dict.get(k)', globals=globals(), number=10000))
+# Стандартный метод впереди.
+print('_____________________ Перебор словаря по значениям: dict vs OrderedDict')
+print(timeit('for v in my_dict: my_dict.values()', globals=globals(), number=10000))
+print(timeit('for v in ord_dict: ord_dict.values()', globals=globals(), number=10000))
+# Стандартный метод впереди.
+"""
+По всей видимости, преимущества OrderedDict на сегодняшний день (после Python 3.6) 
+заключены исключительно в его специфических особенностях, которых нет в обычных 
+словарях (типа, move_to_end() и popitem()).
+"""
